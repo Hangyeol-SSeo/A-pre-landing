c50a02a9eae75a38c7c8b5bbf24a495d48e2bb25
Hangyeol-SSeo <dongha313@gmail.com>
2023-12-14 19:01:00 +0900

    첫페이지 제작

---
+++ src/main/second.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Second() {
+    return (
+        <>
+            <h1 className='text-4xl'>좀 더 쉽게 실력이 맞는 팀과<br/>매너있는 매치를 즐길 수는 없을까?</h1>
+            <p>asdasf</p>
+        </>
+    );
+}
+
+export default Second;
